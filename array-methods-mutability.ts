1dc0caeda3aa14e1c9df009cd6d160ac3c3d033b
Nik Gospodinov <nikola96gospodinov@gmail.com>
2024-09-30 08:07:54 +0100

    made the exercises in the mutability file clearer

--- array-methods-mutability.ts
+++ array-methods-mutability.ts
@@ -1,30 +1,32 @@
 // Exercise 1
 const names = ["Alice", "Bob", "Charlie", "David", "Eve"];
 
-// 1.1 Add a new name to the end of the array
+// 1.1 Add "Justin" to the end of the array
 
-// 1.2 Remove the last name from the array
+// 1.2 Remove the first name from the array
 
-// 1.3 Add a new name to the beginning of the array
+// 1.3 Add "Nina" to the beginning of the array
 
-// 1.4 Remove the first name from the array
+// 1.4 Remove the last name from the array
 
-// 1.5 Reverse the array
+// 1.5 Replace the second name with "Zoe"
 
-// 1.6 Console.log the original array. Is it mutated?
+// 1.6 Reverse the array
+
+// 1.7 Console.log the original array. Is it mutated?
 
 // ---------------------------------------
 
 // Exercise 2
 // const names = ['Alice', 'Bob', 'Charlie', 'David', 'Eve'];
 
-// 2.1 Add a new name to the end of the array without mutating the original array
+// 2.1 Add "Justin" to the end of the array without mutating the original array
 
-// 2.2 Remove the last name from the array without mutating the original array
+// 2.2 Remove the first name from the array without mutating the original array
 
-// 2.3 Add a new name to the beginning of the array without mutating the original array
+// 2.3 Add "Nina" to the beginning of the array without mutating the original array
 
-// 2.4 Remove the first name from the array without mutating the original array
+// 2.4 Remove the last name from the array without mutating the original array
 
 // 2.5 Reverse the array without mutating the original array
 
