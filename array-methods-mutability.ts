c708c432e86c2afb6c6ff1722b1df3d02557d1a3
Nik Gospodinov <nikola96gospodinov@gmail.com>
2024-10-12 11:23:13 +0100

    array mutability more exercises added

--- array-methods-mutability.ts
+++ array-methods-mutability.ts
@@ -15,19 +15,39 @@ const names = ["Alice", "Bob", "Charlie", "David", "Eve"];
 
 // 1.7 Console.log the original array. Is it mutated?
 
+// 1.8 splice()
+
+// 1.8.1 Replace "Charlie" with "Danny"
+
+// 1.8.2 Add "Finn" as the third name in the array
+
+// 1.8.3 Remove "David" from the array using splice()
+
+// 1.8.4 Add "David" back to the end of the array using splice()
+
 // ---------------------------------------
 
 // Exercise 2
 // const names = ['Alice', 'Bob', 'Charlie', 'David', 'Eve'];
 
-// 2.1 Add "Justin" to the end of the array without mutating the original array
+// 2.1 Add "Justin" & "Finn" to the end of the array without mutating the original array
 
-// 2.2 Remove the first name from the array without mutating the original array
+// 2.2 Remove the first and last names from the array without mutating the original array
 
-// 2.3 Add "Nina" to the beginning of the array without mutating the original array
+// 2.3 Add "Nina" & "Tim" to the beginning of the array without mutating the original array
 
-// 2.4 Remove the last name from the array without mutating the original array
+// 2.4 Reverse the array without mutating the original array
 
-// 2.5 Reverse the array without mutating the original array
+// 2.5 Console.log the original array. Is it mutated?
 
-// 2.6 Console.log the original array. Is it mutated?
+// ---------------------------------------
+
+// Exercise 3
+const numbers = [1, 2, 3, 4, 5];
+const bigNumbers = [100, 200, 300, 400, 500];
+
+// 3.1 Merge the two arrays without mutating the original arrays
+
+// 3.2 Add 6 to the end of the merged array without mutating the merged array
+
+// 3.3 Remove the first number from the merged array without mutating the merged arrays
