ca7f168e8e592d6639b9b2ccc76e4acba7e969fa
Nik Gospodinov <nikola96gospodinov@gmail.com>
2024-10-17 14:05:05 +0100

    added string exercises

---
+++ strings.ts
@@ -0,0 +1,35 @@
+// Exercise 1
+const str = "hello world";
+
+// 1.1 Convert the string to uppercase
+
+// 1.2 Convert the string to lowercase
+
+// 1.3 How many characters are in the string?
+
+// 1.4 Get the character at the 5th position
+
+// 1.5 Capitalize the first character of the string
+
+// 1.6 Replace "world" with "everyone"
+
+const str2 = "   Hello World   ";
+
+// 1.7 Remove the whitespace from the beginning and end of the string
+
+// Exercise 2
+
+const emails = [
+  "john.doe@gmail.com",
+  "jane.doe@gmail.com",
+  "jim.beam@gmail.com",
+  "nikki_flintsone@gmail.com",
+  "justin94bieber@gmail.com",
+  "taylor_swift_sucks@gmail.com",
+];
+
+// 2.1 Create usernames for each email. The username is the first part of the email before the @ symbol
+
+// 2.2 Join the usernames with a comma
+
+// 2.3 Create a function that takes a username and returns a string that says "Welcome, [username]!"
