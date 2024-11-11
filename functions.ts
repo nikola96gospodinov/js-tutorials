dc2929995f467c27287af353a4ff6f2ebd9b0d2d
Nik Gospodinov <nikola96gospodinov@gmail.com>
2024-11-11 14:22:56 +0000

    added exercises for functions

---
+++ functions.ts
@@ -0,0 +1,40 @@
+// Exercise 1
+
+// 1.1 Create a function called add that takes two parameters of type number and returns the sum of the two parameters and call it with the numbers 10 and 23
+
+// 1.2 Create a function called subtract that takes two parameters of type number and returns the difference between the two parameters and call it with the numbers 150 and 125
+
+// 1.3 Create a function that takes a parameter of type array of numbers and returns the sum of all the numbers in the array
+
+// 1.4 Create a function that takes a parameter of type array of numbers and returns the average of the numbers in the array
+
+// 1.5 Create a function that takes a parameter of type array of numbers and returns the highest number in the array
+
+// 1.6 Create a function that takes two parameters. 1) parameter of type array of numbers and 2) parameter of type number. The function should return a new array with all the numbers in the original array that are greater than the number passed in
+export const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const number = 5;
+
+// ---------------------------------------
+
+// Exercise 2
+
+// 2.1 Create a function called getFullName that takes a parameter of type object with firstName and lastName properties and returns the full name by concatenating the firstName and lastName properties with a space in between
+
+// 2.2 Call the function with the people array and store the result in a variable called fullNames
+export const people = [
+  { firstName: "John", lastName: "Doe" },
+  { firstName: "Jane", lastName: "Smith" },
+  { firstName: "Emily", lastName: "Johnson" },
+];
+
+// 2.3 Create a function that takes a parameter of type array of strings and returns a new array with all the strings in the original array that start with the letter "a"
+export const strings = ["Apple", "banana", "cherry", "date", "apricot"];
+
+// 2.4 Create a function that takes a parameter of type array of objects with name and age properties and returns a new array with all the objects in the original array that have an age greater than 30
+export const persons = [
+  { name: "John", age: 25 },
+  { name: "Jane", age: 35 },
+  { name: "Emily", age: 30 },
+  { name: "Michael", age: 40 },
+  { name: "Jessica", age: 28 },
+];
